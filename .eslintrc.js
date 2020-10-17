2b605205922471bdd3f3c3a955cc4308bb94d4ec
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-17 09:55:02 +0530

    Add jest rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,18 @@
 module.exports = {
 	root: true,
+
 	parserOptions: {
 		parser: "babel-eslint",
 		sourceType: "module",
 		ecmaVersion: 8
 	},
+
 	env: {
 		browser: true,
 		node: true,
 		"jest/globals": true
 	},
+
 	extends: [
 		"eslint:recommended",
 		"plugin:vue/essential",
@@ -17,10 +20,23 @@ module.exports = {
 		"plugin:jest/recommended",
 		"plugin:jest/style"
 	],
+
 	rules: {
 		"prettier/prettier": 1,
 		camelcase: 1,
 		"no-console": 1,
 		"vue/camelcase": "error"
-	}
+	},
+
+	overrides: [
+		{
+			files: [
+				"**/__tests__/*.{j,t}s?(x)",
+				"**/tests/unit/**/*.spec.{j,t}s?(x)"
+			],
+			env: {
+				jest: true
+			}
+		}
+	]
 };
